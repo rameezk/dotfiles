7f23783e1974004781cbdbe81427783b4fb8b6f5
Rameez Khan <rameezkhan.sa@gmail.com>
2018-12-06 08:39:58 +0200

    Add virtualenvwrapper config

--- python/path.zsh
+++ python/path.zsh
@@ -2,4 +2,9 @@ PATH="/Users/rameezk/.pyenv:$PATH"
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
 
+# Setup virtualenv home
+export WORKON_HOME=$HOME/.virtualenvs
+source /Users/rameezk/.pyenv/versions/3.7.0/bin/virtualenvwrapper.sh
+
+# Tell pyenv-virtualenvwrapper to use pyenv when creating new Python environments
 export PYENV_VIRTUALENVWRAPPER_PREFER_PYVENV="true"
