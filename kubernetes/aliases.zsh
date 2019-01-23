4760c42e436b04d93e281c6d85b8340f7b21ec86
Rameez Khan <rameezkhan.sa@gmail.com>
2019-01-23 14:07:41 +0200

    Change k8s debug image

--- kubernetes/aliases.zsh
+++ kubernetes/aliases.zsh
@@ -17,7 +17,9 @@ alias sk='kubectl -n kube-system'
 alias ke='EDITOR=$EDITOR kubectl edit'
 alias klbaddr="kubectl get svc -ojsonpath='{.status.loadBalancer.ingress[0].hostname}'"
 
-alias kdebug='kubectl run -i -t debug --rm --image=caarlos0/debug --restart=Never'
+# alias kdebug='kubectl run -i -t debug --rm --image=caarlos0/debug --restart=Never'
 alias knrunning='kubectl get pods --field-selector=status.phase!=Running'
 alias kfails='kubectl get po -owide --all-namespaces | grep "0/" | tee /dev/tty | wc -l'
 alias kimg="kubectl get deployment --output=jsonpath='{.spec.template.spec.containers[*].image}'"
+
+alias kdebug='kubectl run --rm -i -t debug --image=rameezk/debuggery --restart=Never'
