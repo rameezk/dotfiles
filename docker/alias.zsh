ab137ee9a4b5720e63381186fae52eac63345687
Rameez Khan <rameezkhan.sa@gmail.com>
2019-06-12 11:07:26 +0200

    Add docker behind proxy alias

--- docker/alias.zsh
+++ docker/alias.zsh
@@ -3,3 +3,6 @@ alias dcb='docker-compose build'
 alias dcd="docker-compose down"
 alias dcu='docker-compose up'
 alias dcr='docker-compose run --rm'
+
+# Docker behind a proxy, assumes http env vars are present
+alias docker-build-behind-proxy='docker build --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy'
