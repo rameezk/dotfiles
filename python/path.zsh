2555bb819a1bf28815982b4d6efb60c1e014f804
Rameez Khan <rameezkhan.sa@gmail.com>
2020-03-23 06:59:36 +0200

    Don't use virtualenvwrapper for now

--- python/path.zsh
+++ python/path.zsh
@@ -8,7 +8,7 @@ export WORKON_HOME=$HOME/.virtualenvs
 if which pipenv >/dev/null 2>&1; then
 	# If neccessary, do some pipenv stuff here
 else
-	source /Users/rameezk/.pyenv/versions/3.7.0/bin/virtualenvwrapper.sh
+#	source /Users/rameezk/.pyenv/versions/3.7.0/bin/virtualenvwrapper.sh
 fi
 
 # Tell pyenv-virtualenvwrapper to use pyenv when creating new Python environments
