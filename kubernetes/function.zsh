c27abae40249e22f4229604656b780e7824ff95d
Rameez Khan <rameezkhan.sa@gmail.com>
2020-03-24 07:36:56 +0200

    Add k8s function to patch an image pull secret

--- kubernetes/function.zsh
+++ kubernetes/function.zsh
@@ -6,3 +6,12 @@ k-delete-pod-with-status() {
 
     k get pods | grep "$1" | awk '{print $1}' | xargs kubectl delete pod
 }
+
+k-patch-img-pull-secret() {
+    if [[ -z "$1" ]]; then
+        echo "Please specify a secret name"
+        return 1
+    fi
+
+    k patch serviceaccount default -p "{\"imagePullSecrets\": [{\"name\": \"$1\"}]}"
+}
