bbeca7f2c75b2cb922e8ff4f93e0653962e316e7
Rameez Khan <rameezkhan.sa@gmail.com>
2020-10-02 06:18:04 +0200

    Org mode agenda files

---
+++ doom-emacs/custom.el
@@ -0,0 +1,15 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("99ea831ca79a916f1bd789de366b639d09811501e8c092c85b2cb7d697777f93" default))
+ '(org-agenda-files
+   '("~/Dropbox/DigitalGarden/todoist.org" "~/Dropbox/DigitalGarden/inbox.org" "/home/rameezk/Dropbox/DigitalGarden/private-2020-09-15.org")))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
