46267fcbff60a4ea8ae3fd2e8764fb15ce33e7b3
Rameez Khan <rameezkhan.sa@gmail.com>
2020-12-05 21:54:56 +0200

    Add init.el

---
+++ emacs/.emacs.d/init.el
@@ -0,0 +1,9 @@
+(setq inhibit-startup-message t)
+
+(scroll-bar-mode -1)        ; Disable visible scrollbar
+(tool-bar-mode -1)          ; Disable the toolbar
+(tooltip-mode -1)           ; Disable tooltips
+(set-fringe-mode 10)        ; Give some breathing room
+
+(menu-bar-mode -1)            ; Disable the menu bar
+(setq visible-bell t)       ; flash screen instead of audible bell
