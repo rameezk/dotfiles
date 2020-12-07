034eb4ee36494893c48d509b8a6d9283c99fe9f8
Rameez Khan <rameezkhan.sa@gmail.com>
2020-12-07 17:16:12 +0200

    Add more Emacs packages

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -12,6 +12,17 @@
 
 (set-face-attribute 'default nil :font "JetbrainsMono Nerd Font" :height 160)
 
+;;
+;;(setq url-proxy-services '(("http" . "http://127.0.0.1:3128")
+;;                           ("https")
+;;  (setq url-proxy-services '(("http" . "http://127.0.0.1:3128")
+;;                             ("https" . "http://127.0.0.1:3128"))
+
+(if (file-exists-p "~/.proxyrc")
+(setq url-proxy-services
+         '(("no_proxy" . "^\\(localhost\\|10.*\\)")
+                ("http" . "127.0.0.1:3128")
+                     ("https" . "127.0.0.1:3128"))) nil)
 
 ;; Make ESC quit prompts
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
@@ -69,6 +80,57 @@
   :init (doom-modeline-mode 1)
   :custom ((doom-modeline-height 15)))
 
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config
+  (setq which-key-idle-delay 0))
+
+(use-package general
+  :config
+  (general-create-definer rune/leader-keys
+    :keymaps '(normal insert visual emacs)
+    :prefix "SPC"
+    :global-prefix "M-SPC")
+
+  (rune/leader-keys
+    "t"  '(:ignore t :which-key "toggles")
+    "tt" '(counsel-load-theme :which-key "choose theme")))
+
+(use-package evil
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  (setq evil-want-C-u-scroll t)
+  (setq evil-want-C-i-jump nil)
+  :config
+  (evil-mode 1)
+  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
+  (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
+
+  ;; Use visual line motions even outside of visual-line-mode buffers
+  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+
+  (evil-set-initial-state 'messages-buffer-mode 'normal)
+  (evil-set-initial-state 'dashboard-mode 'normal))
+
+(use-package evil-collection
+  :after evil
+  :config
+  (evil-collection-init))
+
+(use-package hydra)
+
+(defhydra hydra-text-scale (:timeout 4)
+  "scale text"
+  ("j" text-scale-increase "in")
+  ("k" text-scale-decrease "out")
+  ("f" nil "finished" :exit t))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -77,7 +139,7 @@
  '(custom-safe-themes
    '("628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" default))
  '(package-selected-packages
-   '(doom-modeline counsel color-theme-sanityinc-tomorrow use-package)))
+   '(which-key doom-modeline counsel color-theme-sanityinc-tomorrow use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
