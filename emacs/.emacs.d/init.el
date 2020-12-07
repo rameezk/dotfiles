76a55bb6f4468a0aaa1e6ffe670b7de5343d0cfb
Rameez Khan <rameezkhan.sa@gmail.com>
2020-12-07 06:37:05 +0200

    Trust loading theme

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -38,13 +38,11 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
-(use-package color-theme-sanityinc-tomorrow
-  :ensure t
-  :init
-  :defer t
-  :config
-  (load-theme 'sanityinc-tomorrow-eighties)
-  )
+(use-package color-theme-sanityinc-tomorrow)
+(if (package-installed-p 'color-theme-sanityinc-tomorrow)
+    (load-theme 'sanityinc-tomorrow-eighties t)
+    (load-theme 'wombat t))
+
 
 ;; counsel and ivy for some nice completions
 (use-package counsel)
@@ -76,41 +74,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default bold shadow italic underline bold bold-italic bold])
- '(ansi-color-names-vector
-   (vector "#2d2d2d" "#f2777a" "#99cc99" "#ffcc66" "#6699cc" "#cc99cc" "#66cccc" "#cccccc"))
- '(beacon-color "#f2777a")
  '(custom-safe-themes
    '("628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" default))
- '(fci-rule-color "#515151")
- '(flycheck-color-mode-line-face-to-color 'mode-line-buffer-id)
- '(frame-background-mode 'dark)
- '(global-command-log-mode t)
  '(package-selected-packages
-   '(color-theme-sanityinc-tomorrow doom-modeline counsel ivy command-log-mode use-package))
- '(vc-annotate-background nil)
- '(vc-annotate-color-map
-   '((20 . "#f2777a")
-     (40 . "#f99157")
-     (60 . "#ffcc66")
-     (80 . "#99cc99")
-     (100 . "#66cccc")
-     (120 . "#6699cc")
-     (140 . "#cc99cc")
-     (160 . "#f2777a")
-     (180 . "#f99157")
-     (200 . "#ffcc66")
-     (220 . "#99cc99")
-     (240 . "#66cccc")
-     (260 . "#6699cc")
-     (280 . "#cc99cc")
-     (300 . "#f2777a")
-     (320 . "#f99157")
-     (340 . "#ffcc66")
-     (360 . "#99cc99")))
- '(vc-annotate-very-old-color nil)
- '(window-divider-mode nil))
+   '(doom-modeline counsel color-theme-sanityinc-tomorrow use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
