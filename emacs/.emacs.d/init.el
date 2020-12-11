d60d4ddee53bf305f69428c3e0e34b22bbd651fa
Rameez Khan <rameezkhan.sa@gmail.com>
2020-12-11 17:11:52 +0200

    Add theme

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,5 +1,22 @@
 (set-face-attribute 'default nil :font "JetbrainsMono Nerd Font" :height 160)
 
+(if (package-installed-p 'color-theme-sanityinc-tomorrow)
+    (load-theme 'sanityinc-tomorrow-eighties t)
+    (load-theme 'wombat t))
+
+(if (file-exists-p "~/.proxyrc")
+(setq url-proxy-services
+         '(("no_proxy" . "^\\(localhost\\|10.*\\)")
+                ("http" . "127.0.0.1:3128")
+                     ("https" . "127.0.0.1:3128"))) nil)
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+(package-initialize)
+(unless package-archive-contents
+(package-refresh-contents))
+
 (org-babel-do-load-languages
   'org-babel-load-languages
   '((emacs-lisp . t)))
