c4e43ccd16014d4e837d69f4f4c0f791b7d5a5af
Rameez Khan <rameezkhan.sa@gmail.com>
2020-12-17 07:44:55 +0200

    Add some appearance tweaks

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -10,22 +10,46 @@
                               (time-subtract after-init-time before-init-time)))
                      gcs-done)))
 
-(set-face-attribute 'default nil :font "JetbrainsMono Nerd Font" :height 160)
-
-(load-theme 'wombat t)
-
 (if (file-exists-p "~/.proxyrc")
 (setq url-proxy-services
          '(("no_proxy" . "^\\(localhost\\|10.*\\)")
                 ("http" . "127.0.0.1:3128")
                      ("https" . "127.0.0.1:3128"))) nil)
 
+;; Initialize package sources
+(require 'package)
+
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("melpa-stable" . "https://stable.melpa.org/packages/")
                          ("org" . "https://orgmode.org/elpa/")
                          ("elpa" . "https://elpa.gnu.org/packages/")))
-(package-initialize)
-(unless package-archive-contents
-(package-refresh-contents))
+
+;;(package-initialize)
+;; (unless package-archive-contents
+;;   (package-refresh-contents))
+
+;; Initialize use-package on non-Linux platforms
+(unless (package-installed-p 'use-package) 
+   (package-install 'use-package))
+(require 'use-package)
+
+;; Uncomment this to get a reading on packages that get loaded at startup
+(setq use-package-verbose t)
+
+;; On non-Guix systems, "ensure" packages by default
+(setq use-package-always-ensure t)
+
+(setq inhibit-startup-message t)
+
+(menu-bar-mode -1)
+
+(tool-bar-mode -1)
+
+(scroll-bar-mode -1)
+
+(set-face-attribute 'default nil :font "JetbrainsMono Nerd Font" :height 160)
+
+(load-theme 'wombat t)
 
 (org-babel-do-load-languages
   'org-babel-load-languages
@@ -33,12 +57,12 @@
 
 (push '("conf-unix" . conf-unix) org-src-lang-modes)
 
-;; Automatically tangle our config file when we save it
-(defun rkn/org-babel-tangle-config ()
-  (when (string-equal (file-name-directory (buffer-file-name))
-                      (expand-file-name user-emacs-directory))
-    ;; Dynamic scoping to the rescue
-    (let ((org-confirm-babel-evaluate nil))
-      (org-babel-tangle))))
+;; Since we don't want to disable org-confirm-babel-evaluate all
+;; of the time, do it around the after-save-hook
+(defun rkn/org-babel-tangle-dont-ask ()
+  ;; Dynamic scoping to the rescue
+  (let ((org-confirm-babel-evaluate nil))
+    (org-babel-tangle)))
 
-(add-hook 'org-mode-hook (lambda () (add-hook 'after-save-hook #'rkn/org-babel-tangle-config)))
+(add-hook 'org-mode-hook (lambda () (add-hook 'after-save-hook #'rkn/org-babel-tangle-dont-ask
+                                              'run-at-end 'only-in-org-mode)))
