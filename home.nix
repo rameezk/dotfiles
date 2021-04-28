f976cc3007477b3f45dd9680a034b1166c06abab
Rameez Khan <rameezkhan.sa@gmail.com>
2021-04-28 08:33:04 +0200

    Use gruvbox for vim

--- home.nix
+++ home.nix
@@ -15,12 +15,14 @@
       vim-sensible
       vim-airline
       vim-commentary
+      gruvbox
     ];
     extraConfig = ''
       set background=dark " Better colours for darker backgrounds
       set number "Turn on line numbers
       set cursorline " Highlight the current line the cursor is on
       set visualbell " Turn on visual bell
+      colorscheme gruvbox " Turn on gruvbox colorscheme
     '';
   };
 
