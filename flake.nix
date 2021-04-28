aabc7acaf3356cc08251c0131c12183df1aae725
Rameez Khan <rameezkhan.sa@gmail.com>
2021-04-28 14:13:08 +0200

    Add flakes

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "Rameez's configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+  };
+
+  outputs = { self, ... }@inputs: {
+    homeManagerConfigurations = {
+      darwin = inputs.home-manager.lib.homeManagerConfiguration {
+        configuration = ./hosts/darwin/home.nix;
+        system = "x86_64-linux";
+      };
+    };
+
+  };
+
+}
