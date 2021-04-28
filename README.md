7ba4e5aab1613678db4de15760585072842e965b
Rameez Khan <rameezkhan.sa@gmail.com>
2021-04-28 08:18:07 +0200

    Add prerequisites and update installation instructions

--- README.md
+++ README.md
@@ -5,7 +5,15 @@ Through my progression and learning of the [Nix](https://nixos.org/) ecosystem, 
 
 As such I've declared my old .files as __old-school-thinking__. But you can still find them all [here](https://github.com/rameezk/dotfiles/tree/old-school-thinking).
 
+## Prerequisites
+1. [Nix](https://nixos.org/)
+2. [Home Manager](https://github.com/nix-community/home-manager)
+
 ## Installation
+Clone this repo to `~/.config/dotfiles`
+```sh
+git clone git@github.com:rameezk/dotfiles.git ~/.config/dotfiles
+```
 
 Symlink `home.nix` (not sure if symlinking makes sense yet).
 ```sh
---
+++ home.nix
@@ -0,0 +1,58 @@
+{ config, pkgs, ... }:
+
+{
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "rameezk";
+  home.homeDirectory = "/home/rameezk";
+
+  programs.vim = {
+    enable = true;
+    plugins = with pkgs.vimPlugins; [
+      vim-sensible
+      vim-airline
+      vim-commentary
+    ];
+    extraConfig = ''
+      set background=dark " Better colours for darker backgrounds
+      set number "Turn on line numbers
+      set cursorline " Highlight the current line the cursor is on
+      set visualbell " Turn on visual bell
+    '';
+  };
+
+  # Packages
+  home.packages = with pkgs; [
+    # cli tools
+    ripgrep # faster grepping
+    tmux # terminal multiplexor
+    jq # parsing JSON
+    entr # file watching
+    fzf # fuzzy file finder
+
+    # git
+    git
+    git-crypt # encrypting git repos transparently
+
+    # cloud
+    azure-cli
+
+    # kubernetes
+    kubectl
+    kubectx
+    stern
+  ];
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "21.05";
+}
