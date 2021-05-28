783862300c429329219cda081a05e83725497ee4
Rameez Khan <rameezkhan.sa@gmail.com>
2021-05-28 15:02:33 +0200

    Move git to its own module

--- home.nix
+++ home.nix
@@ -12,6 +12,7 @@ in {
     ./modules/editors/emacs
     ./modules/editors/vim
     ./modules/shell/fish
+    ./modules/vcs/git
     ./modules/language/python
   ];
 
@@ -22,54 +23,6 @@ in {
   # paths it should manage.
   home.username = username;
   home.homeDirectory = homeDirectory;
-
-  # git
-  programs.git = {
-    enable = true;
-    userName = secrets.user.fullName;
-    userEmail = secrets.user.work.emailAddr;
-    aliases = {
-      lg =
-        "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
-      st = "status";
-      br = "branch";
-      dc = "diff --cached";
-      d = "diff";
-      co = "checkout";
-      publish =
-        "!git push --set-upstream origin $(git rev-parse --abbrev-ref HEAD)";
-      delete-branches = ''
-        !f() { git branch | grep -v "master\|main" | xargs git branch -d; }; f'';
-      pr-complete = "!f() { git checkout master && git pull --prune; }; f";
-      generate-ignore =
-        ''!f() { curl -sL "https://www.gitignore.io/api/$1"; }; f'';
-    };
-    includes = [
-      {
-        condition = "gitdir:~/code/personal/";
-        contents = {
-          user = {
-            email = secrets.user.personal.emailAddr;
-            signingkey = secrets.user.personal.gpgFingerprint;
-          };
-        };
-      }
-      {
-        condition = "gitdir:~/.config/";
-        contents = {
-          user = {
-            email = secrets.user.personal.emailAddr;
-            signingkey = secrets.user.personal.gpgFingerprint;
-          };
-        };
-      }
-    ];
-    extraConfig = {
-      user = { signingkey = secrets.user.work.gpgFingerprint; };
-      commit = { gpgsign = true; };
-    };
-    ignores = [ "*~" "*.swp" ".idea/" "*.orig" ".#*" ];
-  };
 
   # bat
   programs.bat = {
@@ -99,11 +52,6 @@ in {
     htop # process manager
     neofetch # nice system info viewer
     fd # a faster find
-
-    # git
-    git
-    git-crypt # encrypting git repos transparently
-    pre-commit # a framework for dealing with git hooks
 
     # cloud
     azure-cli
---
+++ modules/vcs/git/default.nix
@@ -0,0 +1,56 @@
+{ pkgs, ... }:
+
+let secrets = import ./secrets/config.nix;
+in {
+  programs.git = {
+    enable = true;
+    userName = secrets.user.fullName;
+    userEmail = secrets.user.work.emailAddr;
+    aliases = {
+      lg =
+        "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
+      st = "status";
+      br = "branch";
+      dc = "diff --cached";
+      d = "diff";
+      co = "checkout";
+      publish =
+        "!git push --set-upstream origin $(git rev-parse --abbrev-ref HEAD)";
+      delete-branches = ''
+        !f() { git branch | grep -v "master\|main" | xargs git branch -d; }; f'';
+      pr-complete = "!f() { git checkout master && git pull --prune; }; f";
+      generate-ignore =
+        ''!f() { curl -sL "https://www.gitignore.io/api/$1"; }; f'';
+    };
+    includes = [
+      {
+        condition = "gitdir:~/code/personal/";
+        contents = {
+          user = {
+            email = secrets.user.personal.emailAddr;
+            signingkey = secrets.user.personal.gpgFingerprint;
+          };
+        };
+      }
+      {
+        condition = "gitdir:~/.config/";
+        contents = {
+          user = {
+            email = secrets.user.personal.emailAddr;
+            signingkey = secrets.user.personal.gpgFingerprint;
+          };
+        };
+      }
+    ];
+    extraConfig = {
+      user = { signingkey = secrets.user.work.gpgFingerprint; };
+      commit = { gpgsign = true; };
+    };
+    ignores = [ "*~" "*.swp" ".idea/" "*.orig" ".#*" ];
+  };
+
+  home.packages = with pkgs; [
+    git-crypt # encrypting git repos transparently
+    pre-commit # a framework for dealing with git hooks
+  ];
+}
