d53e2f7b0314a88ee185b9383134d52984c75d4f
Rameez Khan <rameezkhan.sa@gmail.com>
2021-06-01 15:25:20 +0200

    Add home.nix for Rivendell

---
+++ machines/rivendell/home.nix
@@ -0,0 +1,36 @@
+{ config, pkgs, lib, ... }:
+
+{
+  # Opt-in to modules by including theme here.
+  #  Is there a better way to do this per machine?
+  imports = [
+    # shell
+    ../../modules/shell
+
+    # editors
+    ../../modules/editors/emacs
+    ../../modules/editors/vim
+
+    # vcs
+    ../../modules/vcs/git
+
+    # lang
+    ../../modules/language/python
+  ];
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  # Packages
+  home.packages = with pkgs; [ nixUnstable nixfmt ];
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "20.09";
+}
