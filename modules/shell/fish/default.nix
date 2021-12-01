8b5aae9697cb3d154e864fd6956a09a5ebb98c37
Rameez Khan <rameezkhan.sa@gmail.com>
2021-12-01 09:08:25 +0200

    Add zlib to path

--- modules/shell/fish/default.nix
+++ modules/shell/fish/default.nix
@@ -47,7 +47,7 @@ in {
       source ~/.proxyrc
 
       # Fix gcc lib issues for certain python libs
-      set -x LD_LIBRARY_PATH /nix/store/9ilyrqidrjbqvmnn8ykjc7lygdd86g7q-gcc-10.2.0-lib/lib
+      set -x LD_LIBRARY_PATH /nix/store/9ilyrqidrjbqvmnn8ykjc7lygdd86g7q-gcc-10.2.0-lib/lib:/nix/store/1l4r0r4ab3v3a3ppir4jwiah3icalk9d-zlib-1.2.11/lib
     '';
 
     shellAbbrs = {
