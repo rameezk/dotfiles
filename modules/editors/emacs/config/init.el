88660d161e026036d68711f748fc0eca5bfd3a80
Rameez Khan <rameezkhan.sa@gmail.com>
2022-01-06 20:31:09 +0200

    Add clojure lsp

--- modules/editors/emacs/config/init.el
+++ modules/editors/emacs/config/init.el
@@ -187,7 +187,20 @@
   :config
   (require 'flycheck-clj-kondo)
   (add-hook 'clojure-mode-hook 'enable-paredit-mode)
-  (add-hook 'clojurescript-mode-hook 'enable-paredit-mode))
+  (add-hook 'clojure-mode-hook 'lsp)
+  (add-hook 'clojurescript-mode-hook 'enable-paredit-mode)
+  (add-hook 'clojurescript-mode-hook 'lsp)
+  (add-hook 'clojurec-mode-hook 'lsp)
+
+  (setq gc-cons-threshold (* 100 1024 1024)
+        read-process-output-max (* 1024 1024)
+        treemacs-space-between-root-nodes nil
+        company-minimum-prefix-length 1
+        lsp-lens-enable t
+        lsp-signature-auto-activate nil
+                                        ; lsp-enable-indentation nil ; uncomment to use cider indentation instead of lsp
+                                        ; lsp-enable-completion-at-point nil ; uncomment to use cider completion instead of lsp
+        ))
 
 (use-package flycheck-clj-kondo)
 
--- modules/language/clojure/default.nix
+++ modules/language/clojure/default.nix
@@ -1,1 +1,9 @@
-{ pkgs, ... }: { home.packages = with pkgs; [ clojure clj-kondo leiningen ]; }
+{ pkgs, ... }: {
+
+  home.packages = with pkgs; [ clojure clj-kondo leiningen clojure-lsp ];
+
+  home.file.".lsp/config.edn".text = ''
+    {:cljfmt {:remove-surrounding-whitespace? false}}
+  '';
+
+}
