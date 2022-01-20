b4a0baca1cc7089b4b0eaadb97abff88406d0c0f
Rameez Khan <rameezkhan.sa@gmail.com>
2022-01-20 08:16:47 +0200

    Add jetbrains module

---
+++ modules/editors/jetbrains/default.nix
@@ -0,0 +1,36 @@
+{ pkgs, ... }:
+
+{
+  home.file.".ideavimrc".text = ''
+    syntax on "Turn on syntax highlighting
+
+    " plugins
+    set surround
+    set commentary
+
+    set iskeyword+=_
+
+    set clipboard+=unnamedplus "use clipboard
+
+    set incsearch "Incremental search
+    set ignorecase
+    set smartcase "Smart case matching
+
+    let mapleader=" "
+
+    " always make these typos
+    :command WQ wq
+    :command Wq wq
+    :command W w
+    :command Q q
+    :command Wall wall
+    :command WAll wall
+
+    " fancy bindings yo
+    :nnoremap <leader>fs :w *<CR>
+    :nnoremap <leader>fS :wall *<CR>
+    :nmap <leader>cl gcc
+    :nnoremap <leader>wv :vsplit <CR>
+    :nnoremap <leader>ws :split <CR>
+  '';
+}
