a21482d82ef164adbf02d2d60c4c8af2a6ee8bfa
Rameez Khan <rameezkhan.sa@gmail.com>
2022-01-24 10:34:50 +0200

    Don't need any extra packages in dotfiles shell.nix
    Its all described in modules/core

--- shell.nix
+++ shell.nix
@@ -2,5 +2,5 @@ let pkgs = import <nixpkgs> { };
 
 in pkgs.mkShell rec {
   name = "dotfiles";
-  buildInputs = with pkgs; [ nixfmt python39 python39Packages.typer ];
+  buildInputs = with pkgs; [ ];
 }
