8f9a1371d6c3427851ac1c22fb50a3bac0a924ce
Rameez Khan <rameezkhan.sa@gmail.com>
2022-01-31 11:01:19 +0200

    Disable clojure-lsp for now

--- modules/language/clojure/default.nix
+++ modules/language/clojure/default.nix
@@ -1,6 +1,11 @@
 { pkgs, ... }: {
 
-  home.packages = with pkgs; [ clojure clj-kondo leiningen clojure-lsp ];
+  home.packages = with pkgs; [
+    clojure
+    clj-kondo
+    leiningen
+    #clojure-lsp 
+  ];
 
   home.file.".lsp/config.edn".text = ''
     {:cljfmt {:remove-surrounding-whitespace? false}}
