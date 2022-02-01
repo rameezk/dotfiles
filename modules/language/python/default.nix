85969df8f64f2589ab1b1b4071e314ac2fe00f3b
Rameez Khan <rameezkhan.sa@gmail.com>
2022-02-01 16:38:19 +0200

    Get latest and greatest versions of pip and pipenv

--- modules/language/python/default.nix
+++ modules/language/python/default.nix
@@ -4,8 +4,21 @@
     # python 3.10 is specified in core module since it is needed to run the dot binary
     # python310
     stdenv.cc.cc.lib
-    python310Packages.pip
+    (python310Packages.pip.overrideAttrs (_: {
+      src = pkgs.python310Packages.fetchPypi {
+        version = "22.0.2";
+        pname = "pip";
+        sha256 = "sha256-J7S3DDTsNfd5R/d3Bw2DMa27HkRIQumOcVDCiNwMrqQ=";
+      };
+    }))
     python310Packages.pipx
+    (pipenv.overrideAttrs (_: {
+      src = pkgs.python310Packages.fetchPypi {
+        version = "2022.1.8";
+        pname = "pipenv";
+        sha256 = "sha256-+E1xGSObIqsqwrj7x9YZ2Dz0ETUgbXKhfE8VHNpSn9A=";
+      };
+    }))
   ];
 
 }
