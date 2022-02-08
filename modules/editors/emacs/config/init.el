d000ffa5c6f077ff64fe67f63b6437486bad4066
Rameez Khan <rameezkhan.sa@gmail.com>
2022-02-08 15:59:27 +0200

    Add org-roam-ui

--- modules/editors/emacs/config/init.el
+++ modules/editors/emacs/config/init.el
@@ -327,6 +327,20 @@
          :file-name "daily/%<%Y-%m-%d>"
          :head "#+TITLE: Daily - %<%A %Y-%m-%d>\n\n* %<%A> %<%Y-%m-%d>")))
 
+(use-package org-roam-ui
+  :straight
+    (:host github :repo "org-roam/org-roam-ui" :branch "main" :files ("*.el" "out"))
+    :after org-roam
+;;         normally we'd recommend hooking orui after org-roam, but since org-roam does not have
+;;         a hookable mode anymore, you're advised to pick something yourself
+;;         if you don't care about startup time, use
+;;  :hook (after-init . org-roam-ui-mode)
+    :config
+    (setq org-roam-ui-sync-theme t
+          org-roam-ui-follow t
+          org-roam-ui-update-on-save t
+          org-roam-ui-open-on-start t))
+
 (setq org-startup-folded t)
 
 (add-hook 'org-mode-hook 'flyspell-mode)
