0af88ac2f1332ccb603a06d32faf529110e6f255
Rameez Khan <rameezkhan.sa@gmail.com>
2022-05-13 08:56:37 +0200

    Add git-crypt to shell.nix

--- shell.nix
+++ shell.nix
@@ -3,6 +3,6 @@ let pkgs = import <nixpkgs> { };
 in pkgs.mkShell rec {
   name = "dotfiles";
   buildInputs = with pkgs; [ 
-(python310.withPackages (ps: with ps; [ typer ]))
+(python310.withPackages (ps: with ps; [ typer ])) git-crypt
 ];
 }
