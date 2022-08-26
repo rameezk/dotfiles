b14138e9a806fa8ae39180556146c373d15b81d3
Rameez Khan <rameezkhan.sa@gmail.com>
2022-08-26 08:31:07 +0200

    Add custom pitch to org headings

--- modules/editors/emacs/config/init.el
+++ modules/editors/emacs/config/init.el
@@ -60,6 +60,19 @@
 (use-package ef-themes
   :config
   (setq ef-themes-to-toggle '(ef-summer ef-winter))
+  (setq ef-themes-headings ; read the manual's entry or the doc string
+        '((0 . (variable-pitch light 1.9))
+          (1 . (variable-pitch light 1.8))
+          (2 . (variable-pitch regular 1.7))
+          (3 . (variable-pitch regular 1.6))
+          (4 . (variable-pitch regular 1.5))
+          (5 . (variable-pitch 1.4)) ; absence of weight means `bold'
+          (6 . (variable-pitch 1.3))
+          (7 . (variable-pitch 1.2))
+          (t . (variable-pitch 1.1))))
+  (setq ef-themes-mixed-fonts t
+        ef-themes-variable-pitch-ui t)
+  (mapc #'disable-theme custom-enabled-themes)
   (load-theme 'ef-winter :no-confirm))
 
 (use-package doom-modeline
