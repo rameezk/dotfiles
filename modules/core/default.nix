72ccce464ab1b0b3def5ae1a5a449c5c74dfc189
Rameez Khan <rameezkhan.sa@gmail.com>
2022-11-11 07:33:55 +0200

    Patch typer so it can play nice Python 3.11

--- modules/core/default.nix
+++ modules/core/default.nix
@@ -2,6 +2,20 @@
   home.packages = with pkgs;
     [
       # Needed to run dot binary
-      (python310.withPackages (ps: with ps; [ typer ]))
+      (python311.withPackages (ps:
+        with ps;
+        [
+          (buildPythonPackage rec {
+            pname = "typer";
+            version = "0.7.0";
+            format = "pyproject";
+            nativeBuildInputs = [ flit-core ];
+            propagatedBuildInputs = [ click ];
+            src = fetchPypi {
+              inherit pname version;
+              sha256 = "sha256-/3l4RleKnyogG1NEKu3rVDMZRmhw++HHAeq2bddoEWU=";
+            };
+          })
+        ]))
     ];
 }
--- shell.nix
+++ shell.nix
@@ -1,9 +1,20 @@
-let pkgs = import <nixpkgs> { };
-
+let
+  pkgs = import <nixpkgs> { };
+  typer_patched = pkgs.python311Packages.buildPythonPackage rec {
+    pname = "typer";
+    version = "0.7.0";
+    format = "pyproject";
+    nativeBuildInputs = [ pkgs.python311Packages.flit-core ];
+    propagatedBuildInputs = [ pkgs.python311Packages.click ];
+    src = pkgs.python311Packages.fetchPypi {
+      inherit pname version;
+      sha256 = "sha256-/3l4RleKnyogG1NEKu3rVDMZRmhw++HHAeq2bddoEWU=";
+    };
+  };
 in pkgs.mkShell rec {
   name = "dotfiles";
   buildInputs = with pkgs; [
-    (python310.withPackages (ps: with ps; [ typer ]))
+    (python311.withPackages (ps: with ps; [ typer_patched ]))
     git-crypt
   ];
 }
