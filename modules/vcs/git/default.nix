c521d7131a00a70659bfa7a6cff10a29972598e5
Rameez Khan <rameezkhan.sa@gmail.com>
2023-06-15 16:06:16 +0200

    Add git alias to get sha

--- modules/vcs/git/default.nix
+++ modules/vcs/git/default.nix
@@ -29,6 +29,7 @@ in {
         "!f() { grep -qxF 'shell.nix' `git rev-parse --show-toplevel`/.git/info/exclude || echo 'shell.nix' >> `git rev-parse --show-toplevel`/.git/info/exclude; }; f";
       rebase-commits-in-branch =
         "!f() { git rebase -i HEAD~$(git cherry -v main | wc -l | xargs); }; f";
+      sha = "rev-parse HEAD";
     };
     includes = [
       {
