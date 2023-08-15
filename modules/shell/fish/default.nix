ecf383f71a2b70bee3509b22a31a619e3fc528dc
Rameez Khan <rameezkhan.sa@gmail.com>
2023-08-15 08:53:02 +0200

    Switch back to fenv for properly sourcing Nix paths

--- modules/shell/fish/default.nix
+++ modules/shell/fish/default.nix
@@ -13,9 +13,13 @@ in {
       # vi mode
       fish_vi_key_bindings
 
+      # Enable fenv for sourcing foreign environment variables. 
+      # This is needed for sourcing the Nix path below.
+      set -p fish_function_path ${pkgs.fishPlugins.foreign-env}/share/fish/vendor_functions.d
+
       # nix
       if test -e ~/.nix-profile/etc/profile.d/nix.sh
-        bass source ~/.nix-profile/etc/profile.d/nix.sh
+        fenv source ~/.nix-profile/etc/profile.d/nix.sh
       end
 
       # Set LANG properly
