0d8dbb81ad8783abfe945b8a715c8b96ce9f5c60
Rameez Khan <rameezkhan.sa@gmail.com>
2024-09-08 13:40:04 +0200

    Clean up inputs

--- flake.nix
+++ flake.nix
@@ -4,8 +4,10 @@
     inputs = {
         nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-        nix-darwin.url = "github:LnL7/nix-darwin";
-        nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+        nix-darwin = {
+            url = "github:LnL7/nix-darwin";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
 
         home-manager = {
             url = "github:rycee/home-manager/master";
@@ -45,38 +47,35 @@
         declarative-cachix.url = "github:jonascarpay/declarative-cachix/master";
     };
 
-    outputs = { self, nixpkgs, nix-darwin, home-manager, emacs-overlay, nixvim, nix-homebrew, homebrew-bundle, homebrew-core, homebrew-cask, sops-nix
-        , declarative-cachix, ... }@inputs:
+    outputs = { self, nixpkgs, ... }@inputs:
         let
             linuxSystems = [ "x86_64-linux" "aarch64-linux" ];
             darwinSystems = [ "aarch64-darwin" "x86_64-darwin" ];
             forAllSystems = f: nixpkgs.lib.genAttrs (linuxSystems ++ darwinSystems) f;
             devShell = system: let pkgs = nixpkgs.legacyPackages.${system}; in {
             default = with pkgs; mkShell {
-                nativeBuildInputs = with pkgs; [ fish git vim age sops ];
-                shellHook = ''
-                export SOPS_AGE_KEY_FILE=~/.config/sops/age/keys.txt
-                '';
+                nativeBuildInputs = with pkgs; [ fish git vim ];
+                shellHook = "";
             };
         };
             mkMachines = { }: {
-                rohan = home-manager.lib.homeManagerConfiguration {
+                rohan = inputs.home-manager.lib.homeManagerConfiguration {
                     configuration = { ... }: {
-                        nixpkgs.overlays = [ emacs-overlay.overlay ];
+                        nixpkgs.overlays = [ inputs.emacs-overlay.overlay ];
                         imports = [ ./machines/rohan/home.nix ];
                     };
                     system = "x86_64-linux";
                     homeDirectory = "/home/rameezk";
                     username = "rameezk";
                 };
-                gondor = home-manager.lib.homeManagerConfiguration {
+                gondor = inputs.home-manager.lib.homeManagerConfiguration {
                     pkgs = import nixpkgs {
                         system = "aarch64-darwin";
-                        overlays = [ emacs-overlay.overlay ];
+                        overlays = [ inputs.emacs-overlay.overlay ];
                     };
                     modules = [
-                        declarative-cachix.homeManagerModules.declarative-cachix
-                        nixvim.homeManagerModules.nixvim
+                        inputs.declarative-cachix.homeManagerModules.declarative-cachix
+                        inputs.nixvim.homeManagerModules.nixvim
                         ./machines/gondor/home.nix
                         {
                             home = {
@@ -93,20 +92,20 @@
             rohan = self.machines.rohan.activationPackage;
             gondor = self.machines.gondor.activationPackage;
 
-            darwinConfigurations."rivendell" = nix-darwin.lib.darwinSystem {
+            darwinConfigurations."rivendell" = inputs.nix-darwin.lib.darwinSystem {
                 system = "aarch64-darwin";
                 specialArgs = inputs;
                 modules = [
-                    home-manager.darwinModules.home-manager
-                    nix-homebrew.darwinModules.nix-homebrew
+                    inputs.home-manager.darwinModules.home-manager
+                    inputs.nix-homebrew.darwinModules.nix-homebrew
                     {
                         nix-homebrew = {
                             user = "rameezk";
                             enable = true;
                             taps = {
-                                "homebrew/homebrew-core" = homebrew-core;
-                                "homebrew/homebrew-cask" = homebrew-cask;
-                                "homebrew/homebrew-bundle" = homebrew-bundle;
+                                "homebrew/homebrew-core" = inputs.homebrew-core;
+                                "homebrew/homebrew-cask" = inputs.homebrew-cask;
+                                "homebrew/homebrew-bundle" = inputs.homebrew-bundle;
                             };
                             mutableTaps = false;
                             autoMigrate = true;
@@ -114,7 +113,10 @@
                     }
                     {
                         home-manager = {
-                            sharedModules = [ nixvim.homeManagerModules.nixvim sops-nix.homeManagerModules.sops ];
+                            sharedModules = [ 
+                                inputs.nixvim.homeManagerModules.nixvim 
+                                inputs.sops-nix.homeManagerModules.sops 
+                            ];
                             users.rameezk = import ./machines/rivendell/home.nix;
                             extraSpecialArgs = { inherit inputs; };
                         };
