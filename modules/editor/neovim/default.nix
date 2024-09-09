e23742046602947fbfd9086955e15d07f815868a
Rameez Khan <rameezkhan.sa@gmail.com>
2024-09-09 15:28:06 +0200

    Enable completion for neovim

--- modules/editor/neovim/default.nix
+++ modules/editor/neovim/default.nix
@@ -39,9 +39,28 @@
                     };
                 };
 
+                cmp-path.enable = true;
+                cmp-nvim-lsp.enable = true;
+                cmp-buffer.enable = true;
                 cmp = {
                     enable = true;
                     autoEnableSources = true;
+                    settings = {
+                        sources = [
+                            { name = "nvim_lsp"; }
+                            { name = "path"; }
+                            { name = "buffer"; }
+                        ];
+                        mapping = {
+                            "<C-Space>" = "cmp.mapping.complete()";
+                            "<C-d>" = "cmp.mapping.scroll_docs(-4)";
+                            "<C-e>" = "cmp.mapping.close()";
+                            "<C-f>" = "cmp.mapping.scroll_docs(4)";
+                            "<CR>" = "cmp.mapping.confirm({ select = true })";
+                            "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+                            "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+                        };
+                    };
                 };
 
                 treesitter = {
