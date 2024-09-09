59578c4fdc143329caf5e8da812495e09572babb
Rameez Khan <rameezkhan.sa@gmail.com>
2024-09-09 12:02:44 +0200

    Add explore and file search mappings to neovim

--- modules/editor/neovim/default.nix
+++ modules/editor/neovim/default.nix
@@ -88,8 +88,18 @@
             keymaps = [
                 {
                     mode = "n";
+                    key = "<leader>fe";
+                    action = "<cmd>Explore<cr>";
+                }
+                {
+                    mode = "n";
                     key = "<leader>ff";
                     action = "<cmd>Telescope find_files<cr>";
+                }
+                {
+                    mode = "n";
+                    key = "<leader>fs";
+                    action = "<cmd>Telescope live_grep<cr>";
                 }
             ];
         };
