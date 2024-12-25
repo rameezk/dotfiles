29dd8fe60cac8bf0800701c05de95b063825c35b
Rameez Khan <rameez@rameezkhan.dev>
2024-12-25 13:52:33 +0200

    Add fangorn machine

---
+++ machines/fangorn/default.nix
@@ -0,0 +1,108 @@
+{ pkgs, ... }:
+
+let
+  user = "rameezk";
+in
+{
+  services.nix-daemon.enable = true;
+
+  nix.settings = {
+    experimental-features = "nix-command flakes";
+  };
+
+  nix.optimise.automatic = true;
+
+  nix.gc = {
+    automatic = true;
+    interval = {
+      Weekday = 3;
+      Hour = 8;
+      Minute = 0;
+    };
+    options = "--delete-older-than 14d";
+  };
+
+  users.users.rameezk = {
+    home = "/Users/rameezk";
+    shell = pkgs.fish;
+    uid = 501;
+  };
+
+  users.knownUsers = [ user ];
+
+  programs.fish.enable = true;
+
+  # enable touchID for sudo
+  security.pam.enableSudoTouchIdAuth = true;
+  environment = {
+    etc."pam.d/sudo_local".text = ''
+      # Managed by Nix Darwin
+      auth       optional       ${pkgs.pam-reattach}/lib/pam/pam_reattach.so ignore_ssh
+      auth       sufficient     pam_tid.so
+    '';
+  };
+
+  system.defaults = {
+    dock = {
+      autohide = true;
+      show-recents = false;
+    };
+
+    # Might require you to logout to take effect
+    NSGlobalDomain = {
+      "com.apple.swipescrolldirection" = false;
+      "com.apple.keyboard.fnState" = true;
+
+      # 120, 90, 60, 30, 12, 6, 2
+      KeyRepeat = 2;
+
+      # 120, 94, 68, 35, 25, 15
+      InitialKeyRepeat = 15;
+    };
+  };
+
+  system.keyboard = {
+    enableKeyMapping = true;
+    remapCapsLockToEscape = true;
+  };
+
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 4;
+
+  # The platform the configuration will be used on.
+  nixpkgs.hostPlatform = "aarch64-darwin";
+
+  homebrew = {
+    # This is a module from nix-darwin
+    # Homebrew is *installed* via the flake input nix-homebrew
+    enable = true;
+
+    casks = [
+      "alfred"
+      "claude"
+      "google-chrome"
+      "keymapp"
+      "nikitabobko/tap/aerospace"
+      "obsidian"
+      "proton-mail"
+      "slack"
+      "todoist"
+      "wezterm"
+      "zoom"
+    ];
+
+    # These app IDs are from using the mas CLI app
+    # mas = mac app store
+    # https://github.com/mas-cli/mas
+    #
+    # $ nix shell nixpkgs#mas
+    # $ mas search <app name>
+    #
+    masApps = {
+      "1password" = 1333542190;
+      "adguard" = 1440147259;
+    };
+  };
+
+}
---
+++ machines/fangorn/home.nix
@@ -0,0 +1,76 @@
+{ inputs, pkgs, ... }:
+
+{
+  imports = [
+    ../../modules
+  ];
+
+  sops = {
+    defaultSopsFile = "${builtins.toString inputs.mysecrets}/secrets.yaml";
+    validateSopsFiles = false;
+
+    age.keyFile = "/Users/rameezk/.config/sops/age/keys.txt";
+
+    secrets = {
+      "private_keys/ssh/rameezk" = {
+        path = "/Users/rameezk/.ssh/id_ed25519";
+        mode = "0600";
+      };
+      "private_keys/gpg/rameezk" = {
+        path = "/Users/rameezk/.config/gpg/rameezk_private.gpg";
+      };
+    };
+  };
+
+  theme.catppuccin = {
+    enable = true;
+    flavour = "frappe";
+  };
+
+  fonts.enable = true;
+
+  shell.enable = true;
+
+  macos.window-management.aerospace.enable = true;
+
+  editor = {
+    neovim.enable = true;
+    jetbrains-vim-mode.enable = true;
+  };
+
+  vcs.git = {
+    enable = true;
+    userName = "Rameez Khan";
+    userEmail = "rameez@rameezkhan.dev";
+    signingKey = "B18F680AF21C6B4A";
+  };
+
+  language = {
+    python.enable = true;
+    java = {
+      enable = true;
+      manageJDK = false;
+    };
+  };
+
+  cloud.aws.enable = true;
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  # Packages
+  home.packages = with pkgs; [
+    nixVersions.latest
+  ];
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "20.09";
+
+}
