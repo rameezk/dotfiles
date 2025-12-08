412d4c8263ed7c5f65adf78f0d6fa896aa350472
Rameez Khan <rameez@rameezkhan.dev>
2025-12-08 16:41:49 +0200

    Update packages

--- modules/vcs/git/default.nix
+++ modules/vcs/git/default.nix
@@ -70,35 +70,38 @@ in
   config = lib.mkIf cfg.enable {
     programs.git = {
       enable = true;
-      userName = cfg.userName;
-      userEmail = cfg.userEmail;
-      aliases = {
-        lg = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
-        st = "status";
-        br = "branch";
-        dc = "diff --cached";
-        d = "diff";
-        co = "checkout";
-        publish = "!git push --set-upstream origin $(git rev-parse --abbrev-ref HEAD)";
-        delete-branches = ''!f() { git branch --merged | grep -v "master\|main" | xargs git branch -d; }; f'';
-        prune-local-branches = "!f() { git branch -vv | grep ': gone' | awk '{print $1}' | fzf -m | xargs git branch -D; }; f";
-        generate-ignore = ''!f() { curl -sL "https://www.gitignore.io/api/$1"; }; f'';
-        open = "!f() { fish -c open_repo_in_browser; }; f";
-        open-pr = "!f() { gh pr view --web; }; f";
-        latest-tag = "!git tag --sort version:refname | tail -n 1";
-        log-to-last-tag = "!f() { git log `git latest-tag`..HEAD --oneline; }; f";
-        ignore-nix-shell = "!f() { grep -qxF 'shell.nix' `git rev-parse --show-toplevel`/.git/info/exclude || echo 'shell.nix' >> `git rev-parse --show-toplevel`/.git/info/exclude; }; f";
-        rebase-commits-in-branch = "!f() { git rebase -i HEAD~$(git cherry -v main | wc -l | xargs); }; f";
-        rb = "!f() { git checkout main; git pull; git checkout -; git rebase main; }; f";
-        rc = "rebase-commits-in-branch";
-        done = "!f() { git checkout main && git pull -p && git prune-local-branches ; }; f";
-        sha = "rev-parse HEAD";
-        create-gh-pr = "!f() { gh pr create -a @me ; }; f";
-        copy-gh-pr-url = "!f() {  gh pr view --json url | jq -r '.url' | xargs echo -n | pbcopy; }; f";
-        commit-into-previous = "commit --amend --no-edit";
-      };
       includes = forAllExtraSigningKeys mkIncludeSettings cfg.extraSigningKeys;
-      extraConfig = {
+      settings = {
+        user = {
+          name = cfg.userName;
+          email = cfg.userEmail;
+          signingkey = cfg.signingKey;
+        };
+        alias = {
+          lg = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
+          st = "status";
+          br = "branch";
+          dc = "diff --cached";
+          d = "diff";
+          co = "checkout";
+          publish = "!git push --set-upstream origin $(git rev-parse --abbrev-ref HEAD)";
+          delete-branches = ''!f() { git branch --merged | grep -v "master\|main" | xargs git branch -d; }; f'';
+          prune-local-branches = "!f() { git branch -vv | grep ': gone' | awk '{print $1}' | fzf -m | xargs git branch -D; }; f";
+          generate-ignore = ''!f() { curl -sL "https://www.gitignore.io/api/$1"; }; f'';
+          open = "!f() { fish -c open_repo_in_browser; }; f";
+          open-pr = "!f() { gh pr view --web; }; f";
+          latest-tag = "!git tag --sort version:refname | tail -n 1";
+          log-to-last-tag = "!f() { git log `git latest-tag`..HEAD --oneline; }; f";
+          ignore-nix-shell = "!f() { grep -qxF 'shell.nix' `git rev-parse --show-toplevel`/.git/info/exclude || echo 'shell.nix' >> `git rev-parse --show-toplevel`/.git/info/exclude; }; f";
+          rebase-commits-in-branch = "!f() { git rebase -i HEAD~$(git cherry -v main | wc -l | xargs); }; f";
+          rb = "!f() { git checkout main; git pull; git checkout -; git rebase main; }; f";
+          rc = "rebase-commits-in-branch";
+          done = "!f() { git checkout main && git pull -p && git prune-local-branches ; }; f";
+          sha = "rev-parse HEAD";
+          create-gh-pr = "!f() { gh pr create -a @me ; }; f";
+          copy-gh-pr-url = "!f() {  gh pr view --json url | jq -r '.url' | xargs echo -n | pbcopy; }; f";
+          commit-into-previous = "commit --amend --no-edit";
+        };
         core = {
           pager = "delta";
         };
@@ -121,9 +124,6 @@ in
         };
         merge = {
           conflictstyle = "diff3";
-        };
-        user = {
-          signingkey = cfg.signingKey;
         };
         commit = {
           gpgsign = true;
